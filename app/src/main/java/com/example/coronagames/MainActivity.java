643738eb16bad23dd3c160a047b729f87a35cce9
configtheworld <erkinture@gmail.com>
2020-04-04 21:51:05 +0300

    esting changes :          edit yapıp push edenler         yorum yazsın sıkıntı ytaşıyan gruba yazsın         iyiakşamlar

--- app/src/main/java/com/example/coronagames/MainActivity.java
+++ app/src/main/java/com/example/coronagames/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /* testing changes :edit yapıp push edenler
+        yorum yazsın sıkıntı ytaşıyan gruba yazsın iyi akşamlar */
     }
+
 }
