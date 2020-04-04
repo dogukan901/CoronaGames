63f6633012d9204b6ebd192aa524180b28f1583f
configtheworld <erkinture@gmail.com>
2020-04-05 00:53:09 +0300

    esting changes :          edit yapıp push edenler         yorum yazsın sıkıntı ytaşıyan gruba yazsın         iyiakşamlar

--- app/src/main/java/com/example/coronagames/MainActivity.java
+++ app/src/main/java/com/example/coronagames/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         /* testing changes :edit yapıp push edenler
         yorum yazsın sıkıntı ytaşıyan gruba yazsın iyi akşamlar */
-    }
+}
 
 }
